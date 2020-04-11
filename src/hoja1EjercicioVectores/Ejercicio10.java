4aaa4c319d510693d5a453c1955816ea8b3e8179
Marta Portolés <0321mportoles@e-itaca.es>
2020-04-11 11:30:09 +0200

    Ejercicios día 10/4

--- src/hoja1EjercicioVectores/Ejercicio10.java
+++ src/hoja1EjercicioVectores/Ejercicio10.java
@@ -3,37 +3,66 @@ package hoja1EjercicioVectores;
 public class Ejercicio10 {
 	public static void main(String[] args)
 	{
-		int suma = 0;
-		int contador = 0;
-		int media;
-		int numeroIgual = 0; int numeroSuperior = 0; int numeroInferior = 0;
-		
-		int numero[] = new int [20];
-		
-		// Rellenamos la matriz
-		for (int x = 0; x < numero.length; x++) {
-			contador = contador + 1;
-			suma = suma + numero[x];
-			media = suma/ contador;	
-			
-			if (numero[x] == media) {
-				numero[x] = numeroIgual;
-			} if (numero [x] < media) {
-				numero[x] = numeroInferior;
-			}else {
-				numero[x] = numeroSuperior;
-			}
-		}
+
+		char[]vectorNumeros = {'1', '2', '3', '4', '5', '6', '7', 'A', 'B', 'C'};
+		char[]vectorPalos = {'O', 'C', 'E', 'P'};
 	
-		media= suma/contador;
-		System.out.println("La media es: "  + media);
-		System.out.println("Hay " + numeroSuperior + " superiores a la media");
-		System.out.println("Hay " + numeroInferior + " infeeriores a la media");
-		System.out.println("Hay " + numeroIgual + " iguales a la media");
-		
+		elegirCarta(vectorNumeros, vectorPalos);
 	}
+
+	public static void elegirCarta (char [] vector1, char [] vector2) {
+		int carta;
+		int palo;
+		
+		carta = (int) Math.floor(Math.random()*vector1.length);
+		palo = (int) Math.floor(Math.random()*vector2.length);
+		
+		for(int x = 1; x <= 40; x++) {
+			System.out.println("La carta elegida es el " + mostrarNumero(vector1[carta]) + " de "+ mostrarPalo(vector2[palo]));
+		}
+	}	
+	
+	public static String mostrarPalo (char caracter) {
+		String cadena;
+		
+		switch (caracter) {
+			case 'O':
+				cadena = "Oros";
+				break;
+			case 'C':
+				cadena = "Copas";
+				break;
+			case 'E':
+				cadena = "Espadas";
+				break;
+			case 'P':
+				cadena = "Bastos";
+				break;
+			default:
+				cadena = "" + caracter;
+		}
+		
+		return cadena;
+	}
+	
+	public static String mostrarNumero (char caracter) {
+		String cadena;
+		switch (caracter) {
+		case 'A':
+			cadena = "Sota";
+			break;
+		case 'B':
+			cadena = "Caballo";
+			break;
+		case 'C':
+			cadena = "Rey";
+			break;
+		
+		default:
+			cadena = "" + caracter;
+			
+		}
+		return cadena;
+	}
+	
 }
-
-
-
-
--- src/hoja1EjercicioVectores/Ejercicio9.java
+++ src/hoja1EjercicioVectores/Ejercicio9.java
@@ -3,7 +3,63 @@ package hoja1EjercicioVectores;
 public class Ejercicio9 {
 	public static void main(String[] args)
 	{
-
-		
+		char[]vectorNumeros = {'1', '2', '3', '4', '5', '6', '7', 'A', 'B', 'C'};
+		char[]vectorPalos = {'O', 'C', 'E', 'P'};
+	
+		elegirCarta(vectorNumeros, vectorPalos);
 	}
+
+	public static void elegirCarta (char [] vector1, char [] vector2) {
+		int carta;
+		int palo;
+		
+		carta = (int) Math.floor(Math.random()*vector1.length);
+		palo = (int) Math.floor(Math.random()*vector2.length);
+		
+		System.out.println("La carta elegida es el " + mostrarNumero(vector1[carta]) + " de "+ mostrarPalo(vector2[palo]));
+	}	
+	
+	public static String mostrarPalo (char caracter) {
+		String cadena;
+		
+		switch (caracter) {
+			case 'O':
+				cadena = "Oros";
+				break;
+			case 'C':
+				cadena = "Copas";
+				break;
+			case 'E':
+				cadena = "Espadas";
+				break;
+			case 'P':
+				cadena = "Bastos";
+				break;
+			default:
+				cadena = "" + caracter;
+		}
+		
+		return cadena;
+	}
+	
+	public static String mostrarNumero (char caracter) {
+		String cadena;
+		switch (caracter) {
+		case 'A':
+			cadena = "Sota";
+			break;
+		case 'B':
+			cadena = "Caballo";
+			break;
+		case 'C':
+			cadena = "Rey";
+			break;
+		
+		default:
+			cadena = "" + caracter;
+			
+		}
+		return cadena;
+	}
+	
 }
